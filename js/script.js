eef02516f9731629851f429c68ccea706e48a4c1
sharphurt <pavellukoanov679@gmail.com>
2023-10-08 20:06:05 +0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+(function(){
+    window.onload = InitializeWindow;
+
+    function InitializeWindow(){
+        var viewPort = GetWindowViewPort();
+        if(viewPort.width > 1400 && viewPort.height > 700) modifyWindowZoom(document.body, viewPort.width / 1440);
+    }
+    function GetWindowViewPort(){
+        return {
+            height: Math.max(document.documentElement.clientHeight, window.innerHeight || 0),
+            width: Math.max(document.documentElement.clientWidth, window.innerWidth || 0)
+        };
+    }
+    function modifyWindowZoom(domElement, percentage){
+        domElement.style["zoom"] = percentage;
+    }
+})();
