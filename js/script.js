1cae76622c19fe16cf39f515d740993f05be180d
sharphurt <pavellukoanov679@gmail.com>
2023-10-09 10:30:01 +0500

    Carousel block fix

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
 (function(){
-   window.onload = InitializeWindow;
+   window.onload = function () {
+       const splineViewerInner = document.querySelector('spline-viewer').shadowRoot;
 
-    function InitializeWindow(){
+       initializeWindow();
+
+       // sorry spline(
+       hideSplineButton(splineViewerInner);
+       scaleToFit(splineViewerInner);
+   }
+
+    function initializeWindow(){
         var viewPort = GetWindowViewPort();
         if(viewPort.width > 1400 && viewPort.height > 700) modifyWindowZoom(document.body, viewPort.width / 1440);
     }
@@ -12,9 +20,18 @@
         };
     }
     function modifyWindowZoom(domElement, percentage){
-        domElement.style["transform"] = "scale(" + percentage + ")";
-        domElement.style["transform-origin"] = "0 0";
+        domElement.style['transform'] = 'scale(' + percentage + ')';
+        domElement.style['transform-origin'] = '0 0';
 
         //domElement.style["zoom"] = percentage;
+    }
+
+    function hideSplineButton(splineInner) {
+        splineInner.querySelector('#logo').remove();
+    }
+
+    function scaleToFit(splineInner) {
+        splineInner.querySelector('#container').style['transform'] = 'scale(0.5)';
+        splineInner.querySelector('#container').style['transform-origin'] = '0 0';
     }
 })();
